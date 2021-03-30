4aa675dd8ba4ac154d0a94edcb790ceba5689ccf
achaves100 <adan.chaves@outlook.com>
2021-03-30 17:42:14 +0000

    Landing Page Complete

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-//include images into your bundle
 import { Navbar } from "./navbar";
 import { Jumbotron } from "./jumbotron";
 import { Card } from "./card";
 import { Footer } from "./footer";
 
-//create your first component
 export function Home() {
 	return (
 		<div className="text-center mt-5">
