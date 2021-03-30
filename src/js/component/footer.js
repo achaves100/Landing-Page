f9ed9e015d91ce58a34572d9fad963a8a026378a
achaves100 <adan.chaves@outlook.com>
2021-03-30 17:39:18 +0000

    Landing Page Complete

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,9 +3,7 @@ import React from "react";
 export function Footer() {
 	return (
 		<footer className="bg-light text-center text-lg-start">
-			<div
-				className="text-center p-3"
-				style="background-color: rgba(0, 0, 0, 0.2);">
+			<div className="text-center p-3">
 				2020 Copyright:
 				<a className="text-dark" href="https://mdbootstrap.com/">
 					MDBootstrap.com
